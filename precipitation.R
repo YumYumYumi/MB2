8af03a95f8619048955d41d5fe23910bcbc9b35e
YumYumYumi <hyunminkang88@gmail.com>
2020-11-10 17:52:29 +0100

    precipitation of Korea (using raster)

--- precipitation.R
+++ precipitation.R
@@ -1,3 +1,7 @@
+getwd() # prints the current working directory
+#setwd("./home/hm/git_environment/MB2")
+#set_dir <- "./home/hm/git_environment/MB2"
+
 library(raster)
 korea<- getData("GADM", country = "KOR", level =2) #get country borders, 
 # other country codes can be found in the manual
@@ -6,9 +10,9 @@ prec_kor <- getData("worldclim", var="prec", res=.5, lon = 127, lat =37) #get pr
 # lon and lat only needed for res =.5
 plot(prec_kor) #plot the precipitation 
 
-prec_kor1 <- crop(prec, korea) # crop precipitation to extent of germany
+prec_kor1 <- crop(prec_kor, korea) # crop precipitation to extent of korea
 spplot(prec_kor1)
-prec_kor2 <- mask(prec_kor1, korea) #mask prec to shape of germany 
+prec_kor2 <- mask(prec_kor1, korea) #mask prec to shape of korea
 spplot(prec_kor2)
 prec_kor2 <- mask(prec_kor1, korea, inverse =T)
 spplot(prec_kor2)
